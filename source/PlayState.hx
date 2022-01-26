be34ec5cfc44e6394b4ab85e784c491c437965f9
HeitorStorm <96298263+HeitorStorm@users.noreply.github.com>
2022-01-25 21:52:23 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -902,6 +902,14 @@ class PlayState extends MusicBeatState
 		add(strumLineNotes);
 		add(grpNoteSplashes);
 
+        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Mod By KWpeter And Storm"), 24);
+        creditTxt.scrollFactor.set();
+        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        creditTxt.borderColor = FlxColor.BLACK;
+        creditTxt.borderSize = 3;
+        creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
+        add(creditTxt);
+
 		if(ClientPrefs.timeBarType == 'Song Name')
 		{
 			timeTxt.size = 24;
